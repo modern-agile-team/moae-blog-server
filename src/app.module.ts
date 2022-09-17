ddf380b0bbf084fbf9d3943a520abb3106146dd2
jiwonpg98 <jiwonpg98@gmail.com>
2022-09-17 23:13:45 +0900

    chore: 필요없는 부분 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaClient } from '@prisma/client';
+import { ConfigModule } from '@nestjs/config';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaClient, ConfigModule.forRoot(), UsersModule],
+  controllers: [AppController, UsersController],
+  providers: [AppService, PrismaService, PrismaClient],
 })
 export class AppModule {}
