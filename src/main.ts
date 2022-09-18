6aa7f10c0b42f251941810c5133964611cde454e
Yonghoon <yonghoonj98@gmail.com>
2022-09-18 14:41:13 +0900

    Setting: [#7] Validation Pipe 옵션 설정

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './config/swagger/swagger';
+import { VALIDATION_OPTIONS } from './config/validation/validation-pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -10,14 +11,10 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const PORT = configService.get<number>('PORT');
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
+  /* Validation Pipe */
+  app.useGlobalPipes(new ValidationPipe(VALIDATION_OPTIONS));
 
+  /* Swagger API Docs */
   setupSwagger(app);
 
   await app.listen(PORT);
