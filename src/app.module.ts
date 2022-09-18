70bc0ce2f14d26e77e20393b90ed327e0d30675d
Yonghoon <yonghoonj98@gmail.com>
2022-09-18 13:59:00 +0900

    feat: [#4] Like Module & Repository 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { BoardsModule } from './boards/boards.module';
 import { CommentsModule } from './comments/comments.module';
+import { LikesModule } from './likes/likes.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { CommentsModule } from './comments/comments.module';
     UsersModule,
     BoardsModule,
     CommentsModule,
+    LikesModule,
   ],
 })
 export class AppModule {}
