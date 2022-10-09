6753954aa80106a81f6e31eab3e151ba279de596
jiwonpg98 <jiwonpg98@gmail.com>
2022-10-09 17:14:40 +0900

    build: 필요없는 라이브러리 삭제

--- src/common/decorators/user.decorator.ts
+++ src/common/decorators/user.decorator.ts
@@ -1,7 +1,4 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-
-const jwtService = new JwtService();
 
 export const User = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
