b672540f066476da71ff9654821a9639552ee82a
Yonghoon <yonghoonj98@gmail.com>
2022-10-16 13:31:45 +0900

    Docs: [#39] Comment Swagger 문서 커스텀 데코레이터화

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -12,8 +12,14 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { comment, Prisma } from '@prisma/client';
+import {
+  DeleteCommentSwagger,
+  GetAllCommentsOnBoardSwagger,
+  PostCommentSwagger,
+  PutCommentSwagger,
+} from 'src/common/decorators/compose-swagger.decorator';
 import { User } from 'src/common/decorators/user.decorator';
 import { CommentService } from './comment.service';
 import { CreateOrUpdateCommentDto } from './dto/create-or-update-comment.dto';
@@ -25,10 +31,7 @@ import { CreateOrUpdateCommentDto } from './dto/create-or-update-comment.dto';
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
-  @ApiOperation({
-    summary: '한개 게시글의 모든 댓글 조회',
-    description: '게시글 고유 번호에 해당하는 모든 댓글을 조회합니다.',
-  })
+  @GetAllCommentsOnBoardSwagger()
   @HttpCode(HttpStatus.OK)
   @Get('all')
   async getAll(
@@ -37,15 +40,7 @@ export class CommentController {
     return await this.commentService.getAll(boardId);
   }
 
-  @ApiOperation({
-    summary: '한개 게시글에 댓글 생성',
-    description: '댓글을 생성하는 API 입니다.',
-  })
-  @ApiBody({
-    type: CreateOrUpdateCommentDto,
-    description: '댓글 내용을 입력해 주세요. 길이 최소 1 ~ 최대 500',
-    required: true,
-  })
+  @PostCommentSwagger()
   @HttpCode(HttpStatus.CREATED)
   @Post()
   async create(
@@ -56,15 +51,7 @@ export class CommentController {
     return await this.commentService.create(userId, boardId, context);
   }
 
-  @ApiOperation({
-    summary: '댓글 수정',
-    description: '댓글을 수정하는 API 입니다.',
-  })
-  @ApiBody({
-    type: CreateOrUpdateCommentDto,
-    description: '수정할 댓글 내용을 입력해 주세요. 길이 최소 1 ~ 최대 500',
-    required: true,
-  })
+  @PutCommentSwagger()
   @HttpCode(HttpStatus.OK)
   @Put(':commentId')
   async update(
@@ -75,10 +62,7 @@ export class CommentController {
     return await this.commentService.update(userId, commentId, context);
   }
 
-  @ApiOperation({
-    summary: '한 개의 댓글 삭제',
-    description: '한개 댓글을 삭제합니다.',
-  })
+  @DeleteCommentSwagger()
   @HttpCode(HttpStatus.OK)
   @Delete(':commentId')
   async delete(
