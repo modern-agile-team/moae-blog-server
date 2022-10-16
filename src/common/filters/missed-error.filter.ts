eba3dba61f8b1061598e85943b4a39bb2ccbc328
Yonghoon <yonghoonj98@gmail.com>
2022-10-16 14:45:35 +0900

    Setting: [#26] 예외 처리 필터 초기 코드 세팅

--- src/common/filters/missed-error.filter.ts
+++ src/common/filters/missed-error.filter.ts
@@ -2,25 +2,23 @@ import {
   ExceptionFilter,
   Catch,
   ArgumentsHost,
-  HttpException,
   Logger,
   HttpStatus,
 } from '@nestjs/common';
 import { Response } from 'express';
 import { HttpArgumentsHost } from '@nestjs/common/interfaces';
-import { Prisma } from '@prisma/client';
 
-@Catch(Prisma.PrismaClientUnknownRequestError)
-export class PrismaUnKnownFilter implements ExceptionFilter {
+@Catch()
+export class MissedErrorFilter implements ExceptionFilter {
   constructor(private readonly logger: Logger) {}
 
-  catch(exception: HttpException, host: ArgumentsHost) {
+  catch(exception: any, host: ArgumentsHost): void {
     const ctx: HttpArgumentsHost = host.switchToHttp();
     const response: Response = ctx.getResponse<Response>();
 
     this.logger.error(exception);
 
-    return response
+    response
       .status(HttpStatus.INTERNAL_SERVER_ERROR)
       .json({ message: '서버 에러입니다.' });
   }
--- src/main.ts
+++ src/main.ts
@@ -4,23 +4,21 @@ import { ConfigService } from '@nestjs/config';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './config/swagger/swagger';
 import { VALIDATION_OPTIONS } from './config/validation/validation-pipe';
-import { PrismaKnownFilter } from './common/filters/prisma-known.filter';
-import { PrismaUnKnownFilter } from './common/filters/prisma-known.filter copy';
+import { moaeBlogErrorFilters } from './common/filters/export-filters';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const logger: Logger = new Logger();
   const configService = app.get(ConfigService);
-  const PORT = configService.get<number>('PORT');
+  const PORT: number = configService.get<number>('PORT');
 
   app.enableCors();
 
   /* Validation Pipe */
   app.useGlobalPipes(new ValidationPipe(VALIDATION_OPTIONS));
-  app.useGlobalFilters(
-    new PrismaKnownFilter(logger),
-    new PrismaUnKnownFilter(logger),
-  );
+
+  /* Error Filter */
+  app.useGlobalFilters(...moaeBlogErrorFilters(logger));
 
   /* Swagger API Docs */
   setupSwagger(app);
