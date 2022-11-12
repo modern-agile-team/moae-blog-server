5ce68a4da897816b247c244d763be93fb4e6071e
Yonghoon <yonghoonj98@gmail.com>
2022-11-12 18:33:21 +0900

    Feature: 이미 있는 카테고리면 기존 카테고리 정보 리턴 및 없는 카테고리면 생성한 카테고리 정보 리턴

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -60,7 +60,6 @@ export class BoardController {
       createBoardDto,
     );
     await this.categoryOnBoardService.create(categories, board.id);
-
     return board;
   }
 
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -6,9 +6,27 @@ export class CategoryService {
   constructor(private readonly categoryRepository: CategoryRepository) {}
 
   async create(categories: string[]) {
-    const mappedRequest = categories.map((category: string) =>
+    /*
+    이 부분은.. 로직을 따로 설명드릴게요.
+    대충 설명하자면 카테고리를 받아서 기존에 있는 카테고리들은 새로 만들지 않고 
+    없으면 새로 만든 후에 있는거랑 새로 만든거랑 합쳐서 반환합니다.
+    */
+    const mappedOriginCategory = categories.map((category: string) =>
+      this.categoryRepository.find(category),
+    );
+    const originCategories = (await Promise.all(mappedOriginCategory)).filter(
+      (category) => category,
+    );
+    const filteredCategories = categories.filter(
+      (category) =>
+        originCategories.filter(
+          (orginCategory) => orginCategory.name === category,
+        ).length === 0,
+    );
+    const mappedRequest = filteredCategories.map((category: string) =>
       this.categoryRepository.create(category),
     );
-    return await Promise.all(mappedRequest);
+    const newCategories = await Promise.all(mappedRequest);
+    return [...originCategories, ...newCategories];
   }
 }
