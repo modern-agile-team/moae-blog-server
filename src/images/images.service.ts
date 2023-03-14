6844388fa3134a25cea6490920c71cf6ac727347
jiwonpg98 <jiwonpg98@gmail.com>
2023-03-15 00:41:54 +0900

    chore: 이미지 업로드 시 반환값 cdn 주소 추가

--- src/images/images.service.ts
+++ src/images/images.service.ts
@@ -2,23 +2,30 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { BoardService } from '../board/board.service';
 import { UploadThumbnailDto } from './dto/upload-thumbnail.dto';
 import { UpdateBoardDto } from '../board/dto/update-board.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class ImagesService {
-  constructor(private readonly boardService: BoardService) {}
+  private readonly cdnUrl: string;
+  constructor(
+    private readonly boardService: BoardService,
+    private readonly configService: ConfigService,
+  ) {
+    this.cdnUrl = this.configService.get('AWS_CLOUDFRONT_URL');
+  }
 
   uploadFile(files: { files: Express.MulterS3.File[] }) {
     if (!files) {
       throw new BadRequestException('no file exist');
     }
-    return { names: files.files.map(({ key }) => key.split('/').at(-1)) };
+    return { names: files.files.map(({ key }) => this.cdnUrl + '/' + key) };
   }
 
   async uploadThumbnail(dto: UploadThumbnailDto) {
     if (!dto.thumbnail) {
       throw new BadRequestException('no file exist');
     }
-    const thumbnail = dto.thumbnail.key.split('/').at(-1);
+    const thumbnail = this.cdnUrl + '/' + dto.thumbnail.key.split('/').at(-1);
     await this.boardService.update(dto, <UpdateBoardDto>{ thumbnail });
     return { name: thumbnail };
   }
